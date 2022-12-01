0a11ca9a124a7a709d58d43b3949f900bb850c65
Ahmad Barclay <ahmad.barclay@ons.gov.uk>
2022-12-01 14:45:35 +0000

    embed fix

--- src/utils.js
+++ src/utils.js
@@ -125,7 +125,7 @@ export function deleteStorage(name) {
 }
 
 export function makeEmbed(code) {
-  return `<iframe id="censusquiz" src="https://www.ons.gov.uk/visualisations/cenusquiz/" width="100%" height="600px" frameborder="0" allow="clipboard-write" allowfullscreen></iframe>
+  return `<iframe id="censusquiz" src="https://www.ons.gov.uk/visualisations/censusquiz/" width="100%" height="600px" frameborder="0" allow="clipboard-write" allowfullscreen></iframe>
 <script>(function(){let f=document.getElementById("censusquiz");f.src=f.src+"?parentUrl="+document.location.href${code ? `+"#${code}"` : ''};})()</script>`;
 }
 
